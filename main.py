7383c4854e4eac638abf81b1c85d8646b53a0665
WASENIER <139533385+WASENIER@users.noreply.github.com>
2023-07-14 11:15:48 +0000

    added api function to main.py

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
-JOBS = [
-    {"title": "Software Engineer", "location": "Seattle, WA", "salary": "$120,000"},
-    {"title": "Data Analyst", "location": "New York, NY", "salary": "$80,000"},
-    {"title": "Web Developer", "location": "San Francisco, CA", "salary": "$100,000"},
-    {"title": "Project Manager", "location": "London, UK", "salary": "£50,000"},
-    {"title": "Graphic Designer", "location": "Paris, France", "salary": "€40,000"}
-]
+JOBS = [{
+  "title": "Software Engineer",
+  "location": "Seattle, WA",
+  "salary": "$120,000"
+}, {
+  "title": "Data Analyst",
+  "location": "New York, NY",
+  "salary": "$80,000"
+}, {
+  "title": "Web Developer",
+  "location": "San Francisco, CA",
+  "salary": "$100,000"
+}, {
+  "title": "Project Manager",
+  "location": "London, UK",
+  "salary": "£50,000"
+}, {
+  "title": "Graphic Designer",
+  "location": "Paris, France",
+  "salary": "€40,000"
+}]
 
 
 @app.route("/")
 def hello_world():
   return render_template('home.html', jobs=JOBS, company_name='Wassen')
+
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)

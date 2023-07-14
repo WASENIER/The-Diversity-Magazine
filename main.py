c8257db5b48ce1ac9fed55c173ba3f42a748b0ef
WASENIER <139533385+WASENIER@users.noreply.github.com>
2023-07-14 10:39:44 +0000

    Used for loop to display available jobs from list.
     <h2 class="text-center">Available Positions</h2>
        <!-- special flask framework templating syntax to insert dynamic data, refer to main.py to see the list -->
     <div>
    <!-- create a for loop and attach variables from list-->
        {% for job in jobs %}
        <div style="margin-bottom:16px;">
          <h4 style="color:rgb(120,120,120); margin-bottom:4px;">{{job['title']}}</h4>
          <div><b>Location:</b>{{job['location']}}</div>
          <!--use if function to prevent empty output for salary-->
          {%if 'salary' in job%}
          <div><b>Salary:</b>{{job['salary']}}</div>
          {% endif %}
        </div>
        {% endfor %}
    </div>

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [
+    {"title": "Software Engineer", "location": "Seattle, WA", "salary": "$120,000"},
+    {"title": "Data Analyst", "location": "New York, NY", "salary": "$80,000"},
+    {"title": "Web Developer", "location": "San Francisco, CA", "salary": "$100,000"},
+    {"title": "Project Manager", "location": "London, UK", "salary": "£50,000"},
+    {"title": "Graphic Designer", "location": "Paris, France", "salary": "€40,000"}
+]
+
+
 @app.route("/")
 def hello_world():
-  return render_template('home.html')
+  return render_template('home.html', jobs=JOBS, company_name='Wassen')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
